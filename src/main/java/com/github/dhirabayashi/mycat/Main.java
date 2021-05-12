ff3554ffd8df67d12835c0b6b6247b7dc955d9fd
daiki <gigabowser@hotmail.co.jp>
2021-05-12 19:37:32 +0900

    ファイルの存在チェックを追加

--- src/main/java/com/github/dhirabayashi/mycat/Main.java
+++ src/main/java/com/github/dhirabayashi/mycat/Main.java
@@ -7,7 +7,12 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws IOException {
         for(var arg : args) {
-            System.out.println(cat(Path.of(arg)));
+            var file = Path.of(arg);
+            if(!Files.exists(file)) {
+                System.err.println("mycat: " + file + ": No such file or directory");
+                continue;
+            }
+            System.out.println(cat(file));
         }
     }
 
